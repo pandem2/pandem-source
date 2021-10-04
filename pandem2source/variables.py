14bb146955841e5d5a1de9f0011257ab6d0cce2d
forchard <forchard@protonmail.com>
2021-10-04 03:56:42 +0200

    Adding json formating to list of varianles and reference to source datasets

--- pandem2source/variables.py
+++ pandem2source/variables.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import pkg_resources
 import json
+import codecs
 
 def read_variables_xls():
   path = pkg_resources.resource_filename("pandem2source", "data/list-of-variables.xlsx")
   df = pd.read_excel(path)
-  df = df.rename(columns = {"Variable":"variable", "Data Family":"data_family", "Linked Attributes":"linked_attributes", "Aliases":"aliases", "Description":"description", "Type":"type", "Unit":"unit"})
+  df = df.rename(columns = {"Variable":"variable", "Data Family":"data_family", "Linked Attributes":"linked_attributes", "Aliases":"aliases", "Description":"description", "Type":"type", "Unit":"unit", "Datasets":"datasets"})
   for col in df.columns:
     if col != "description":
       df[col] = df[col].str.lower().str.replace(", ", ",").str.replace(".", "").str.replace(" ", "-")
-    if col == "linked_attributes":
+    if col == "linked_attributes" or col == "datasets":
       df[col] = df[col].str.split(",")
     if col == "aliases":
       df[col] = df[col].apply(json.loads)
@@ -18,4 +19,11 @@ def read_variables_xls():
 def write_json_variables():
   df = read_variables_xls()
   path = pkg_resources.resource_filename("pandem2source", "data/list-of-variables.json")
-  df.to_json(path, orient = "index")
+  result = df.to_json(orient = "records")
+  parsed = json.loads(result)
+  j = json.dumps(parsed, indent=2)
+
+  file = codecs.open(path, "w", "utf-8")
+  file.write(j)
+  file.close()
+
