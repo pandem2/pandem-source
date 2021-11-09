fa1f31a3bbe1b3de359d7148ef083448d4e03e4a
Charline CLAIN <c.clain@epiconcept.fr>
2021-11-09 15:20:46 +0100

    Migrating DataframeReader to new object oriented

--- pandem2source/dfreader.py
+++ pandem2source/dfreader.py
@@ -1,38 +1,37 @@
-import pykka
 import os
 import time
 import threading
+from . import worker
+from abc import ABC, abstractmethod, ABCMeta
 
 
-class DataframeReader(pykka.ThreadingActor):
-    
+class DataframeReader(worker.Worker):
+    __metaclass__ = ABCMeta
 
-    def __init__(self, name, orchestrator_ref, storage_ref, settings): 
-        super(DataframeReader, self).__init__()
-        self.name = name
-        self.orchestrator_proxy = orchestrator_ref.proxy()
+    def __init__(self, name, orchestrator_ref, storage_ref, settings):
+        super().__init__(name = name, orchestrator_ref = orchestrator_ref, settings = settings)
+        self.orchestrator_proxy = orchestrator_ref.proxy() 
         self.storage_proxy = storage_ref.proxy()
-        self.settings = settings
         print(f'here in {self.name} __init__')
-      
-       
+
+
     def on_start(self):
-       
-        threading.Thread(target=self.actor_loop).start()
+        super().on_start()
         print(f'here in {self.name} on-start')
 
 
     def read_df_start(self, dls):
-       
+
         pass
-        
+
 
     def send_heartbeat(self):
+        #already implemented for parent class. To delete
         self.orchestrator_proxy.get_heartbeat(self.name)
-    
-    
+
+
     def actor_loop(self):
         while True:
-            
+
             self.send_heartbeat()
             time.sleep(20)
--- pandem2source/orchestrator.py
+++ pandem2source/orchestrator.py
@@ -9,14 +9,14 @@ import os
 
 
 class Orchestration(pykka.ThreadingActor):
-    
+
     def __init__(self, settings):
         super(Orchestration, self).__init__()
         self.settings = settings
         self.current_actors = dict()
-        
-   
-    def on_start(self):      
+
+
+    def on_start(self):
         #lauch storage_actor
         storage_ref = storage.Storage.start('storage', self.actor_ref, self.settings)
         self.current_actors['storage'] = {'ref': storage_ref}
@@ -44,22 +44,14 @@ class Orchestration(pykka.ThreadingActor):
                 dls_label = [dls for dls in dls_dicts if dls['acquisition']['channel']['name'] == label]
                 for dls in dls_label:
                     acquisition_proxy.add_datasource(dls)
-                self.current_actors['acquisition_'+label] = {'ref': acquisition_ref, 'sources': dls_label} 
+                self.current_actors['acquisition_'+label] = {'ref': acquisition_ref, 'sources': dls_label}
         print('in orchestrator on-start')
-        
-        
+
+
     def get_heartbeat(self, actor_name):
         now = datetime.datetime.now()
         self.current_actors[actor_name]['heartbeat'] = now
         print(f'heartbeat from {actor_name} at: {now}')
-        
+
     def get_actor(self, actor_name):
         return self.current_actors[actor_name]['ref']
-
-
-        
-    
-
-
-
-    
