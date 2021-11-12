720831eaa31740a9ebca0eb1682021250b93454e
Audrey <a.lopes@epiconcept.fr>
2021-11-12 15:07:44 +0100

    standardizer

--- pandem2source/__main__.py
+++ pandem2source/__main__.py
@@ -21,6 +21,12 @@ def main(a):
     help="Whether to output debugging messages to the console", 
   )
   
+  start_parser.add_argument(
+    "--no-acquire", 
+    action="store_true", 
+    help="Whether to output debugging messages to the console", 
+  )
+  
   start_parser.set_defaults(func = do_start)
   
   # setup 
@@ -52,6 +58,11 @@ def main(a):
     args.func(args, parser)
 
 # handlers
+def do_start_dev(debug = True, no_acquire = False):
+  from types import SimpleNamespace
+  return do_start(SimpleNamespace(**{"debug":True, "no_acquire":True}))
+
+# handlers
 def do_start(args, *other):
   if args.debug:
     root = logging.getLogger()
@@ -66,7 +77,8 @@ def do_start(args, *other):
   defaults = os.path.join(pkg_dir, "data/defaults.yml") 
   with open(defaults, "r") as f:
       settings = yaml.safe_load(f)
-  orchestrator_ref = Orchestration.start(settings)
+  orchestrator_ref = Orchestration.start(settings, start_acquisition = not args.no_acquire)
+  return orchestrator_ref.proxy()
   
 def do_reset(args, *other):
   if args.variables:
@@ -77,3 +89,5 @@ def do_reset(args, *other):
 
 if __name__ == "__main__":
   main(sys.argv[1] if len(sys.argv)>1 else None)
+
+
