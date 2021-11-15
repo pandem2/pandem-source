cb0e02d9f3eec606cba3d8570463cc4aa975f6c0
Audrey <a.lopes@epiconcept.fr>
2021-11-15 15:20:35 +0100

    migration de la methode get_referential() dans variables

--- pandem2source/standardizer.py
+++ pandem2source/standardizer.py
@@ -14,6 +14,9 @@ class Standardizer(worker.Worker):
     def on_start(self):
         self._storage_proxy=self._orchestrator_proxy.get_actor('storage').get().proxy()
         print(f'here in {self.name} on-start')
+        self._varaibles_proxy=self._orchestrator_proxy.get_actor('variables').get().proxy()
+        print(f'here in {self.name} on-start')
+
           
     def get_variables(self): 
         var_list=self._storage_proxy.read_files('variables/variables.json').get()
@@ -27,44 +30,10 @@ class Standardizer(worker.Worker):
                     dic_variables[alias['alias']]=alias_var
         return dic_variables
 
-    def get_referential(self,variable_name):
-        #variable_name = nom du dossier = geo or diseases or country_code
-        list_files=[]
-        referentiel=[]
-        path=os.path.join(os.getenv('PANDEM_HOME'), 'files/variables/', variable_name)
-        if os.path.isdir(path):
-            list_files=self._storage_proxy.list_files(path).get()
-            for file in list_files:
-                var_list=self._storage_proxy.read_files(file['path']).get()
-                for var in var_list['tuples']:
-                    referentiel.append(var)
-        else: 
-            return None
-
-        return referentiel
-
     #def standardize(self, tuples_to_validate, file_name, job_id):
     def standardize(self,file_name, job_id):
-        #tuples_to_validate = tuples
+        #tuples_to_validate = tuples fichier passé en parametre  
         #file_name = nom du référentiel à recupérer
         #job_id = nom de l'attribu à tester
+        pass
 
-        tuples=self._storage_proxy.read_files('variables/covid19-datahub-sample-tuples.json').get()
-        referential=self.get_referential(file_name)
-        list_ref=[]
-
-        for ref in referential: 
-            if ref['attr'] not in list_ref:
-                list_ref.append(ref['attr'])
-            else:
-                pass
-
-        for var in tuples['tuples']: 
-            one_tuple=var['attrs'][job_id]
-            if one_tuple in list_ref: 
-                print(one_tuple,'existe')
-            else:
-                #Création d'un objet issue
-                print('la maladie ', one_tuple, 'nexiste pas')
-        
-        return None
