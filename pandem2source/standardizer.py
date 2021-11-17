b4f5f8363fdf9be3fbdc553792eaa0a13a048f2b
Audrey <a.lopes@epiconcept.fr>
2021-11-17 18:25:16 +0100

    1- Modification du fichier des varaibles excel 2- Modification du country_name avec country_code 3- changement du separateur pour le nom des variables

--- pandem2source/standardizer.py
+++ pandem2source/standardizer.py
@@ -4,6 +4,7 @@ import time
 import threading
 from . import worker
 from datetime import datetime
+import copy
 
 
 class Standardizer(worker.Worker):
@@ -15,46 +16,68 @@ class Standardizer(worker.Worker):
     def on_start(self):
         self._storage_proxy=self._orchestrator_proxy.get_actor('storage').get().proxy()
         print(f'here in {self.name} on-start')
-        self._varaibles_proxy=self._orchestrator_proxy.get_actor('variables').get().proxy()
+        self._variables_proxy=self._orchestrator_proxy.get_actor('variables').get().proxy()
         print(f'here in {self.name} on-start')
 
-          
-    def get_variables(self): 
-        var_list=self._storage_proxy.read_files('variables/variables.json').get()
-        for var in var_list: 
-            dic_variables[var['variable']]=var
-            if 'aliases' in dic_variables :
-                for alias in dic_variables['aliases']:
-                    alias_var=var.clone()
-                    alias_var['variable']=alias['alias']
-                    alias_var['modifiers']=alias['modifiers']
-                    dic_variables[alias['alias']]=alias_var
-        return dic_variables
-
-    #def standardize(self, tuples_to_validate, var_name, job):
-    def standardize(self,var_name):  
+    #def standardize(self, tuples_to_validate, job):
+    def standardize(self):  
         job={'job_id':123, 'step':'step-job'}
+        line_number='12'
         tuples=self._storage_proxy.read_files('variables/covid19-datahub-sample-tuples.json').get()
-        referential=self._varaibles_proxy.get_referential(var_name).get()
+        std_tuples=copy.deepcopy(tuples)
+        variables=self._variables_proxy.get_variables().get()
         list_issues=[]
         list_ref=[]
-        list_ref=set([x['attr'] for x in referential])
+        refsalias=[]
+        refs_alias={}
+        refs_values={}
 
+        type_translate=['referential_alias']
+        type_validate=['referential', 'referential_alias']
+        
         for var in tuples['tuples']:
-            one_tuple=var['attrs'][var_name]
-            if one_tuple in list_ref: 
-                None
-            else:
-                message=(f"Code {one_tuple} does not exist in referential '{var_name}'. Line {var['attrs']['line_number']} in file {tuples['scope']['file_name']} (source: '{tuples['scope']['source']}').")
-                issue={ job['step'], 
-                        var['attrs']['line_number'], 
-                        tuples['scope']['source'], 
-                        tuples['scope']['file_name'], 
-                        message, 
-                        datetime.now(), 
-                        job['job_id'], 
-                        "ref-not-found"}
-                list_issues.append(issue)
-                #si aucune erreur on retourne la liste des tuples
-        return list_issues
+            std_var=copy.deepcopy(var)
+            for var_name in var['attrs'].keys():
+                if var_name not in refs_values and variables[var_name]['type'] in type_validate:
+                    referential=self._variables_proxy.get_referential(var_name).get()
+                    refs_values[var_name]=set([x['attr'] for x in referential])
+                if var_name not in refs_alias and variables[var_name]['type'] in type_translate: 
+                    alias=self._variables_proxy.get_referential(var_name).get() 
+                    code=variables[var_name]['linked_attributes'][0]
+                    refs_alias[var_name] = dict([(x['attr'],x['attrs'][code]) for x in alias])
+                var_value=var['attrs'][var_name]
 
+                if var_value in refs_values[var_name]: 
+                    if var_name in refs_alias : 
+                        std_var['attrs'].pop(var_name)
+                        std_var['attrs'][code]=refs_alias[var_name][var_value]
+                        
+                        std_tuples['tuples'].append(std_var)
+                        del std_tuples['tuples'][std_tuples['tuples'].index(var)]
+                else:
+                    message=(f"Code {var_value} does not exist in referential '{var_name}'. Line {line_number} in file {tuples['scope']['file_name']} (source: '{tuples['scope']['source']}').")
+                    issue={ job['step'], 
+                            line_number, 
+                            tuples['scope']['source'], 
+                            tuples['scope']['file_name'], 
+                            message, 
+                            datetime.now(), 
+                            job['job_id'], 
+                            "ref-not-found"}
+                    list_issues.append(issue)
+            else:
+                None
+        for var in std_tuples['tuples']:
+            var['attrs']['source']=tuples['scope']['source']
+            var['attrs']['file_name']=tuples['scope']['file_name']
+            var['attrs']['sent_on']=tuples['scope']['sent_on']
+            var['attrs']['sent_by']=tuples['scope']['sent_by']
+            for global_var in std_tuples['scope']['globals']:
+                pass
+                #var['attrs']['global_var['variables']']=global_var
+                #print (global_var)
+
+        if not(list_issues): 
+            return std_tuples
+        else: 
+            return list_issues
