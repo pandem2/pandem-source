44522e8485686c43bf6ce796201c142df1a3631c
Huan <1552988785@qq.com>
2023-02-05 13:49:48 +0000

    caculations

--- pandemsource/data/indicators/excess_mortality_ltcf/function.R
+++ pandemsource/data/indicators/excess_mortality_ltcf/function.R
@@ -1,1 +1,4 @@
-excess_mortality_pscore *  5
+#excess_mortality_pscore *  5
+library(dplyr)
+library(p2synthr)
+synth1(excess_mortality_pscore, group_names=c("mortality","other"), group_prob=c(0.5,0.5), setSeed = T, seedValue = 10)$mortality
--- pandemsource/data/indicators/population_with_obesity/function.R
+++ pandemsource/data/indicators/population_with_obesity/function.R
@@ -1,4 +1,4 @@
 #round(population * 0.02, 0)
 library(dplyr)
 library(p2synthr)
-synth1(population, group_names=c("diabetes","obesity","kidney","respiratory","underlying", "other"), group_prob=c(0.066,0.08,0.015,0.06,0.05,0.729), setSeed = T, seedValue = 10)$obesity
+round(synth1(population/10000, group_names=c("diabetes","obesity","kidney","respiratory","underlying", "other"), group_prob=c(0.066,0.08,0.015,0.06,0.05,0.729), setSeed = T, seedValue = 10)$obesity*10000)
