cc47af963330308db194c8e275c9c81810bfb3e3
forchard <forchard@protonmail.com>
2023-03-01 12:33:24 +0100

    fixing issue on gamma wave arriging too earl on Netherland (simulated data)

--- pandemsource/data/indicators/confirmed_cases_alpha/function.R
+++ pandemsource/data/indicators/confirmed_cases_alpha/function.R
@@ -1,13 +1,17 @@
 library(Pandem2simulator)
-df0 = data.frame(time = reporting_period, cases = confirmed_cases)
+ratio = 60000 / max(confirmed_cases)
+df0 = data.frame(time = reporting_period, cases = round(confirmed_cases * ratio))
 df <- Pandem2simulator::fx_simulator(df0,startdate = "2023-10-15")
 gamma <- df[df$variant == "gamma" & !is.na(df$cases) & df$cases > 0,]$cases
+gamma <- round(gamma / ratio)
 
 l = nrow(df0)
 ind_start_date = nrow(df0[df0$time < "2023-10-15",])
 seq_zeros = sample(0, size = ind_start_date, replace = TRUE)
 cases_fin = c(seq_zeros,gamma)
 cases_fin = cases_fin[0:l]
+cases_fin <- ifelse(is.na(cases_fin), 0, cases_fin)
+cases_fin <- sapply(1:length(confirmed_cases), function(i) min(confirmed_cases[[i]], cases_fin[[i]]))
 
 alpha <- confirmed_cases - cases_fin
 alpha <- ifelse(is.na(alpha) | (alpha < 0), 0, alpha)
--- pandemsource/data/indicators/confirmed_cases_gamma/function.R
+++ pandemsource/data/indicators/confirmed_cases_gamma/function.R
@@ -1,7 +1,9 @@
 library(Pandem2simulator)
-df0 = data.frame(time = reporting_period, cases = confirmed_cases)
+ratio = 60000 / max(confirmed_cases)
+df0 = data.frame(time = reporting_period, cases = round(confirmed_cases * ratio))
 df <- Pandem2simulator::fx_simulator(df0,startdate = "2023-10-15")
 gamma <- df[df$variant == "gamma" & !is.na(df$cases) & df$cases > 0,]$cases
+gamma <- round(gamma / ratio)
 
 l = nrow(df0)
 ind_start_date = nrow(df0[df0$time < "2023-10-15",])
@@ -9,6 +11,6 @@ seq_zeros = sample(0, size = ind_start_date, replace = TRUE)
 cases_fin = c(seq_zeros,gamma)
 cases_fin = cases_fin[0:l]
 cases_fin <- ifelse(is.na(cases_fin), 0, cases_fin)
-print(cases_fin)
+cases_fin <- sapply(1:length(confirmed_cases), function(i) min(confirmed_cases[[i]], cases_fin[[i]]))
 cases_fin
 
