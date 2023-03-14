dca80ef5416843e6e702accecc2ec7ff71b83919
forchard <forchard@protonmail.com>
2023-03-14 02:05:45 +0100

    fixing again issues on variant for fx

--- pandemsource/data/indicators/confirmed_cases_alpha/function.R
+++ pandemsource/data/indicators/confirmed_cases_alpha/function.R
@@ -1,4 +1,4 @@
-start_date <- "2023-10-25"
+start_date <- "2023-11-25"
 if (geo_code == "NL" || geo_code == "DE") {
   library(Pandem2simulator)
   library(dplyr)
@@ -8,32 +8,15 @@ if (geo_code == "NL" || geo_code == "DE") {
   all_cases <- data.frame(time = as_date(reporting_period), confirmed_cases = confirmed_cases)
   all_cases$cases <- round(all_cases$confirmed_cases * ratio)
   df <- Pandem2simulator::fx_simulator(data = all_cases,startdate = start_date)
-  df <- df %>% 
-    rowwise() %>%
-    mutate(round = round(cases / ratio))%>%
-    mutate(time = as_date(time))%>% 
-    group_by(time)%>% 
-    mutate(somme = sum(round))%>% 
-    mutate(moins = somme - confirmed_cases)%>%
-    select(-c("cases","somme"))
   
-  gamma <- df %>% filter(variant =='gamma') %>%  mutate(variant ="Gamma") %>% rowwise() %>% mutate(round = round - moins)
-  other <- df %>% filter(variant !='gamma') %>% mutate(variant ="Alpha")
-  
-  df = union_all(gamma,other) %>% 
-    group_by(time)%>% 
-    mutate(somme = sum(round))%>% 
-    mutate(moins = somme - confirmed_cases)%>%
-    rowwise()%>%
-    mutate(cases = ifelse(variant == "other",round - moins,round))%>%
-    arrange(as_date(time))%>% select(-c("moins","somme","confirmed_cases","round"))
-  
-  zero_cases <- all_cases %>% filter(as_date(time) < as_date(min(df$time))) %>% mutate(variant = "Alpha") %>% select(-cases)%>% rename(cases = confirmed_cases) %>% select(time,variant,cases)
-  
-  all = all_cases %>% mutate(variant = "All")%>% select(-cases)%>% rename(cases = confirmed_cases)%>% select(time,variant,cases)
-  df = union_all(zero_cases,df)
-  df = union_all(df,all)
-  (df %>% filter(variant == "Alpha"))$cases
+  gamma = df[df$variant == "gamma",]$cases
+  na_gamma = rep(NA, length(reporting_period[reporting_period < start_date]))
+  mapping = sapply(1:(length(reporting_period)-length(na_gamma)), function(i) floor(i*length(gamma)/(length(reporting_period)-length(na_gamma))))
+  mapped_gamma = c(na_gamma, sapply(1:length(mapping), function(i) round(gamma[[i]]/ratio)))
+  mapped_gamma[[length(na_gamma)+1]] = 1
+  final_gamma = sapply(1:length(confirmed_cases), function(i) if(is.na(mapped_gamma[[i]])) NA else{ if(confirmed_cases[[i]] < mapped_gamma[[i]]) confirmed_cases[[i]] else mapped_gamma[[i]]})
+  alpha = confirmed_cases - ifelse(is.na(final_gamma), 0, final_gamma)
+  alpha 
 } else {
   ratio = ifelse(reporting_period < start_date, NA , atan(-10 + as.numeric(difftime(strptime(reporting_period, "%Y-%m-%d"), start_date, unit ='days'))^0.52)/ pi + 0.5)
   gamma = round(confirmed_cases * ratio)
