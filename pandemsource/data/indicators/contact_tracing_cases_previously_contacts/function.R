dbd2d6318542c517894fbae90d5bc2814bb86bbc
epi-ccn <c.clain@epiconcept.fr>
2023-05-03 15:33:32 +0200

    P2-87 Create data for the CT page(update 03/05/23)

--- pandemsource/data/indicators/contact_tracing_cases_previously_contacts/function.R
+++ pandemsource/data/indicators/contact_tracing_cases_previously_contacts/function.R
@@ -1,55 +1,62 @@
 library(dplyr)
 
 ### Local access to the dataset loading dataframe
-"""
+
 setwd("C:/Users/Charline CLAIN/Documents/DataScience/pandem")
 df <- read.csv("timeserie-2023-04-18_13-45-19.csv")
-"""
 
 ###  Variables initialization
-
-reporting_period <- df$date[df$ts_id == 1]
+period <- df$date[df$ts_id == 1]
 confirmed_cases <- df$value[df$ts_id == 1]
-number_of_contact_tracers <- sapply(reporting_period, function(v) 1000)
-full_contact_tracing <-  sample(df$value[df$ts_id == 4], length(reporting_period)) # sur mon export -> ts 4
-limited_tracing <-   sample(df$value[df$ts_id == 3],length(reporting_period)) # sur mon export -> ts 3
-no_contact_tracing <-   sample(df$value[df$ts_id == 2] , length(reporting_period))# sur mon export -> ts 2
+nb_contact_tracers = rep(10, length(period))
+full_ctp <- ifelse(confirmed_cases < 1000, 1, 0)
+partial_ctp <- ifelse(confirmed_cases > 1000 & confirmed_cases < 10000,1, 0)
+no_ctp <- ifelse(confirmed_cases > 10000,1,0)
 
-day_loop <- function(period, confirmed_cases, df){ #sans recalcul des nouveaux cas
-  nb_contact_tracers = 10
-  ct_capacity_val = nb_contact_tracers * 100
-  df_res <- data.frame(period,ct_capacity_val, confirmed_cases)
-  df_res <- arrange(df_res,period)
+day_loop <- function(period, confirmed_cases, nb_contact_tracers,full_ctp, partial_ctp, no_ctp){
+  ret <- list()
+  cases_unreached <- 0
+  contacts_unreached <- 0
  
-  ### Cas
- 
-  for (i in 1:nrow(df_res)){df_res$cases_reached_day[i]<-min(df_res[i,3],df_res[i,2])} #calcul des nouveaux cas reached in the day
-  for (i in 1:nrow(df_res)){df_res$cases_unreached_day[i]<-(df_res[i,3]-df_res[i,4])}# calcul des nouveaux cas UNreached in the day
-  for (i in 1:nrow(df_res)){df_res$capacity_reeval[i]<-(df_res[i,2]-df_res[i,4])}#calcul de la capacite restante
-  for (i in 1:nrow(df_res)){df_res$cases_reached_with_delay_if_capa[i]<-min(df_res[i-1,5],df_res[i,6])} # cases reached with delay if capacity
-  # ligne 38 : attention valeur fausse pour la premiere ligne car on n'a pas de valeur
-  for (i in 1:nrow(df_res)){df_res$cases_unreached_with_delay[i]<-(df_res[i-1,5]-df_res[i,7])}# cas en retard non contactés
-  for (i in 1:nrow(df_res)){df_res$capacity_reeval_again[i]<-(df_res[i,6]-df_res[i,7])}#recalcul de la capacite restante
-  for (i in 1:nrow(df_res)){df_res$sum_cases_reached[i]<-(df_res[i,4]+df_res[i,7])}#Somme des cas contactés dans la journée
-  for (i in 1:nrow(df_res)){df_res$sum_cases_unreached[i]<-(df_res[i,5]+df_res[i,8])}#Somme des cas non contactés dans la journée
- 
-  ### Contacts
-  for (i in 1:nrow(df_res)){df_res$sum_contacts_identified[i]<-(df_res[i,10]*sample(5:6,1))}#Somme des contacts identifies
-  for (i in 1:nrow(df_res)){df_res$contacts_reached_day[i]<-min(df_res[i,9],df_res[i,12])} #calcul des nouveaux contacts reached in the day
-  for (i in 1:nrow(df_res)){df_res$contacts_unreached_day[i]<-(df_res[i,12]-df_res[i,13])}# calcul des nouveaux contacts UNreached in the day
- 
-  for (i in 1:nrow(df_res)){df_res$capacity_reeval_contact[i]<-(df_res[i,9]-df_res[i,13])}#calcul de la capacite restante
-  for (i in 1:nrow(df_res)){df_res$contacts_reached_with_delay_if_capa[i]<-min(df_res[i-1,14],df_res[i,15])} # contacts reached with delay if capacity
-  for (i in 1:nrow(df_res)){df_res$contacts_unreached_with_delay[i]<-(df_res[i-1,14]-df_res[i,16])}# contacts en retard non contactés
- 
-  for (i in 1:nrow(df_res)){df_res$capacity_reeval_again2[i]<-(df_res[i,15]-df_res[i,16])}#recalcul de la capacite restante
-  for (i in 1:nrow(df_res)){df_res$sum_contacts_reached[i]<-(df_res[i,13]+df_res[i,16])}#Somme des contacts contactés dans la journée
-  for (i in 1:nrow(df_res)){df_res$sum_contacts_unreached[i]<-(df_res[i,14]+df_res[i,17])}#Somme des contacts non contactés dans la journée
- 
-### Cases previously identified as contacts --> NaN en resultat à revoir
-for (i in 1:nrow(df_res)){df_res$sum_cases_prev_contacts[i]<-(df_res[i,13]*(df_res[i,11]/df_res[i,3]))}# contact_tracing_contacts_identified *(number of cases reached within a day / confirmed_cases)
-return(df_res$sum_cases_prev_contacts)
-
+  for (i in 1:length(period)){
+    ct_cap = nb_contact_tracers[i] * 500
+    if(full_ctp[i]==1){new_cases_to_reach=confirmed_cases[i]}
+    else if(partial_ctp[i]==1){new_cases_to_reach = round(mean(confirmed_cases[i],ct_cap)* round(runif(1, 0.95, 1.05), 2))}
+    else if(no_ctp[i]==1){new_cases_to_reach =0}
+   
+    cases_reached_day <- min(new_cases_to_reach,ct_cap) #calcul des    nouveaux cas reached in the day
+    cases_unreached_day <- new_cases_to_reach - cases_reached_day #    calcul des nouveaux cas UNreached in the day
+    ct_cap <- ct_cap - cases_reached_day #calcul de la capacite restante
+   
+    cases_reached_with_delay<-min(ct_cap, cases_unreached)
+    cases_unreached_with_delay <- cases_unreached - cases_reached_with_delay # cas en retard non contactés
+    ct_cap <- ct_cap - cases_reached_with_delay #calcul de la capacite restante
+   
+   
+    cases_reached <- cases_reached_day + cases_reached_with_delay#Somme des cas contactés dans la journée
+    cases_unreached <- cases_unreached_day + cases_unreached_with_delay#Somme des cas non contactés dans la journée
+   
+   
+    sum_contacts_identified<-cases_unreached*sample(5:6,1)#Somme des contacts identifies
+    contacts_reached_day<-min(ct_cap,sum_contacts_identified) #calcul des nouveauxcontacts reached in the day
+    contacts_unreached_day<-(sum_contacts_identified-contacts_reached_day)# calcul des nouveauxcontacts UNreached in the day
+   
+    ct_cap<-(ct_cap-contacts_reached_day)#calcul de la capacite restante
+    contacts_reached_with_delay_if_capa<-min(ct_cap,contacts_unreached) #contacts reached with delay if capacity
+    contacts_unreached_with_delay<-(contacts_unreached-contacts_reached_with_delay_if_capa)# contacts enretard non contactés
+   
+    ct_cap<-ct_cap - contacts_reached_with_delay_if_capa#recalcul de la capaciterestante
+    contacts_reached<-contacts_reached_day+contacts_reached_with_delay_if_capa #Somme des contacts contactésdans la journée
+    contacts_unreached<-contacts_unreached_day + contacts_unreached_with_delay #Somme des contacts noncontactés dans la journée
+    contact_tracing_cases_previously_contacts = round(sum_contacts_identified *(cases_reached/confirmed_cases[i]))
+    ret[[i]] <- list(date = period[[i]],cases_to_reach = new_cases_to_reach,
+                     cases_reached_day=cases_reached_day,cases_unreached_day=cases_unreached_day,
+                     cases_reached=cases_reached, cases_unreached=cases_unreached,
+                     sum_contacts_identified=sum_contacts_identified,contacts_reached_day=contacts_reached_day,
+                     contacts_reached=contacts_reached,contacts_unreached=contacts_unreached, ct_cap=ct_cap,
+                     contact_tracing_cases_previously_contacts=contact_tracing_cases_previously_contacts)
+  }
+  df = do.call(rbind, ret)
+  return (df$contact_tracing_cases_previously_contacts)
 }
-
-df_res <- day_loop(reporting_period,limited_tracing,df )
+df_res <- day_loop(period,confirmed_cases, nb_contact_tracers,full_ctp, partial_ctp, no_ctp)
