34a52103cac8f7c9fe9966b333ff46baea2692b6
forchard <forchard@protonmail.com>
2023-05-30 16:31:01 +0200

    fixing NA on confirmed cases for ecdc variants

--- pandemsource/data/scripts/py/sources/ecdc_covid19_variants.py
+++ pandemsource/data/scripts/py/sources/ecdc_covid19_variants.py
@@ -1,10 +1,11 @@
 import pandas as pd
+import numpy as np
 
 def df_transform(df):
   dfvar = df.groupby(['country', 'country_code', 'year_week', 'variant']).agg(
     {"number_detections_variant":"sum", "percent_cases_sequenced":"max", "line_number":"min"}
   ).reset_index()
-  dfvar["confirmed_cases"] = round(dfvar["number_detections_variant"] / (dfvar["percent_cases_sequenced"] / 100))
+  dfvar["confirmed_cases"] = np.where(dfvar["percent_cases_sequenced"]>0, round(dfvar["number_detections_variant"] / (dfvar["percent_cases_sequenced"] / 100)), None)
   del dfvar["percent_cases_sequenced"]
   dfall = df.groupby(['country', 'country_code', 'year_week']).agg({"new_cases":"max", "number_sequenced":"max", "line_number":"min"}).reset_index()
   dfall["not_sequenced"] = dfall["new_cases"] - dfall["number_sequenced"]
